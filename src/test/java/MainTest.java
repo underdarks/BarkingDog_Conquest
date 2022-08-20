8b2667dd5d0203a1d3ab7a76a76dc7c8e6d4435e
underdarks <rkdrl45617@gmail.com>
2022-08-20 16:48:46 +0900

    초기화 설정

---
+++ src/test/java/MainTest.java
@@ -0,0 +1,16 @@
+import org.junit.jupiter.api.Test;
+
+public class MainTest {
+
+    @Test
+    public void t(){
+        //given
+        System.out.println("true = " + true);
+
+
+        //when
+
+
+        //then
+    }
+}
